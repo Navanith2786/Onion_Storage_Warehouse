a68fac5555022209a3bdc5b935064d49aaf21d39
Navanith.S.R <navanithsenthil@gmail.com>
2026-02-26 14:36:35 +0530

    updated

--- onion_warehouse_project/onion_warehouse_project/backend/models.py
+++ onion_warehouse_project/onion_warehouse_project/backend/models.py
@@ -6,8 +6,8 @@ db = SQLAlchemy()
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(80), unique=True, nullable=False)
-    password_hash = db.Column(db.String(120), nullable=False)
+    username = db.Column(db.String(150), unique=True, nullable=False)
+    password_hash = db.Column(db.String(512), nullable=False)
 
     def set_password(self, password):
         self.password_hash = generate_password_hash(password)
