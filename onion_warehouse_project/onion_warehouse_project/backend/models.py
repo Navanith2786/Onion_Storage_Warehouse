b0c0e69dde13966f7d830f9acc8cfc2b1e25e167
Navanith.S.R <navanithsenthil@gmail.com>
2026-02-26 15:29:50 +0530

    Add files via upload

--- onion_warehouse_project/onion_warehouse_project/backend/models.py
+++ onion_warehouse_project/onion_warehouse_project/backend/models.py
@@ -23,15 +23,22 @@ class SensorReading(db.Model):
     nh3 = db.Column(db.Float)
     ben = db.Column(db.Float)
     total_ppm = db.Column(db.Float)
+    cooler_on = db.Column(db.Boolean, default=False)
     timestamp = db.Column(db.DateTime, default=lambda: datetime.now(timezone.utc))
 
     def to_dict(self):
         return {
             "temp": self.temp,
+            "T": self.temp,
             "hum": self.hum,
+            "H": self.hum,
             "co2": self.co2,
+            "CO2": self.co2,
             "nh3": self.nh3,
+            "NH3": self.nh3,
             "ben": self.ben,
+            "Ben": self.ben,
             "total": self.total_ppm,
+            "c": 1 if self.cooler_on else 0,
             "time": self.timestamp.strftime("%H:%M")
         }
