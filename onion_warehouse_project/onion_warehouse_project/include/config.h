27938962349fde713365be98882a2d249d0f1ae3
Navanith.S.R <navanithsenthil@gmail.com>
2026-02-26 13:27:03 +0530

    updated

---
+++ onion_warehouse_project/onion_warehouse_project/include/config.h
@@ -0,0 +1,17 @@
+#ifndef CONFIG_H
+#define CONFIG_H
+
+// WiFi Credentials
+const char* WIFI_SSID = "POCO X7 Pro";
+const char* WIFI_PASS = "88888888";
+
+// Backend Configuration
+// Replace with Render URL after deployment
+const char* BACKEND_URL = "https://onion-storage-warehouse-3.onrender.com/api/data"; 
+const char* API_KEY = "onion-warehouse-secure-key-2024";
+
+// Telegram Configuration
+const char* BOTtoken = "7973350052:AAFAeiBgf7xCSTboEKE3ezJx1QtJqE_ZlHQ";
+const char* CHAT_ID = "1774441961";
+
+#endif
